b94742e6366cba741e8f8246dae097debb42b1ef
cyhao <95aying@gmail.com>
2019-05-12 14:09:16 +0800

    do nothing

--- TRkeras/main_server.py
+++ TRkeras/main_server.py
@@ -19,16 +19,16 @@ print('train shapes:', Xtrain.shape, Ytrain.shape)
 print('valid shapes:', Xvalid.shape, Yvalid.shape)
 
 #from transformer_with_input_emb import Transformer, LRSchedulerPerStep
-from .transformer import Transformer, LRSchedulerPerStep
+from TRkeras.transformer import Transformer, LRSchedulerPerStep
 
 d_model = 256
 s2s = Transformer(itokens, otokens, len_limit=70, d_model=d_model, d_inner_hid=512, \
                   n_head=8, layers=2, dropout=0.1)
 
-mfile = './models/8kmodel.h5'
+mfile = './models/ckpt-{epoch:02d}-val_accu_{val_accu:.5f}.h5'
 
 lr_scheduler = LRSchedulerPerStep(d_model, 4000)
-model_saver = ModelCheckpoint(mfile, save_best_only=True, save_weights_only=True)
+model_saver = ModelCheckpoint(mfile, save_weights_only=True,period=1)
 
 s2s.compile(Adam(0.001, 0.9, 0.98, epsilon=1e-9))
 try:
@@ -68,7 +68,7 @@ if 'test' in sys.argv:
         fgen.write(str(np.mean(acc)))
 else:
     s2s.model.summary()
-    s2s.model.fit([Xtrain, Ytrain], None, batch_size=50, epochs=30, \
+    s2s.model.fit([Xtrain, Ytrain], None, batch_size=50, epochs=40, \
                   validation_data=([Xvalid, Yvalid], None), \
                   callbacks=[lr_scheduler, model_saver])
      # val_accu @ 30 epoch: 0.7045
